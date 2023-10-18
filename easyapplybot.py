26702233278ffaf60e54d0e2f2bef979a2f9562b
Conrad <conrad.rzuc@gmail.com>
2023-10-18 22:10:47 +0100

    unused
    VSC editor highlights those imports as unused, most likely an artefact from the past

--- easyapplybot.py
+++ easyapplybot.py
@@ -1,19 +1,16 @@
 from __future__ import annotations
-import time, random, os, csv, platform
+import time, random, os, csv
 import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import pandas as pd
 import pyautogui
-
-from urllib.request import urlopen
 from webdriver_manager.chrome import ChromeDriverManager
 import re
 import yaml
